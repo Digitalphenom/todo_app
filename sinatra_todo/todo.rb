d963cdb663f94ed983fc68f7b70a248dcb88818c
digitalphenom <digitalphenom0@gmail.com>
2025-01-28 13:44:16 -0800

    refactored error validation

--- sinatra_todo/todo.rb
+++ sinatra_todo/todo.rb
@@ -1,8 +1,8 @@
-require "sinatra"
-require "sinatra/reloader"
-#require "tilt/erubis"
+require 'sinatra'
+require 'sinatra/reloader'
+# require "tilt/erubis"
 
-configure do 
+configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
 end
@@ -11,34 +11,40 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
 # View list of lists
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
   erb :lists
 end
 
 # Render new list form
-get "/lists/new" do 
+get '/lists/new' do
   erb :new_list
 end
 
-# Create new list
-post "/lists" do
-  list_name = params[:list_name].strip
-  if (1..100).cover? list_name.size
-    session[:lists] << {name: list_name, todos: []}
-    session[:success] = "The list has been created!"
-    redirect "/lists"
-  elsif session[:lists].any? { |list| list[:name] == list_name }
-    session[:error] = "List name must be unique."
-    erb :new_list, layout: :layout
-  else
-    session[:error] = "List name must be between 1 and 100 characters"
-    erb :new_list, layout: :layout
+def validate(list)
+  if !(1..100).cover? list.size
+    'List name must be between 1 and 100 characters'
+  elsif session[:lists].any? { |hsh| hsh[:name] == list }
+    'List name must be unique.'
   end
 end
 
+# Create new list
+post '/lists' do
+  list_name = params[:list_name].strip
+  error = validate(list_name)
+
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created!'
+    redirect '/lists'
+  end
+end
