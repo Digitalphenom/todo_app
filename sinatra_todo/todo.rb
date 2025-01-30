f4bb83bcc7a8d0efe14194747a0dcbaf6d558b73
digitalphenom <digitalphenom0@gmail.com>
2025-01-30 13:00:45 -0800

    added remove todo functionality

--- sinatra_todo/todo.rb
+++ sinatra_todo/todo.rb
@@ -12,6 +12,24 @@ before do
   session[:lists] ||= []
 end
 
+#‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
+
+def validate(list)
+  if !(1..100).cover? list.size
+    'List name must be between 1 and 100 characters'
+  elsif session[:lists].any? { |hsh| hsh[:name] == list }
+    'List name must be unique.'
+  end
+end
+
+def error_for_todo(name)
+  if !(1..100).cover? name.size
+    'Todo must be between 1 and 100 characters'
+  end
+end
+
+#‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
+
 get '/' do
   redirect '/lists'
 end
@@ -29,17 +47,26 @@ get '/lists/new' do
   erb :new_list
 end
 
-def validate(list)
-  if !(1..100).cover? list.size
-    'List name must be between 1 and 100 characters'
-  elsif session[:lists].any? { |hsh| hsh[:name] == list }
-    'List name must be unique.'
-  end
+# Edit existing todo
+get '/lists/:id/edit' do
+  @list_id = params[:id].to_i
+  @lists = session[:lists][@list_id]
+  erb :edit_list
 end
+
+# Visit curent todo
+get '/lists/:id' do
+  @list_id = params[:id].to_i
+  @lists = session[:lists][@list_id]
+  erb :list
+end
+
+#‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧‧
 
 # Create new list
 post '/lists' do
   list_name = params[:list_name].strip
+
   error = validate(list_name)
   if error
     session[:error] = error
@@ -51,20 +78,47 @@ post '/lists' do
   end
 end
 
-# Edit existing todo
+# Add todo item
+post '/lists/:list_id/todos' do
+  @list_id = params[:list_id].to_i
+  @lists = session[:lists][@list_id]
+  text = params[:todo].strip
 
-get '/lists/:id/edit' do
-  id = params[:id].to_i
-  @lists = session[:lists][id]
-  erb :edit_list
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list
+  else
+    @lists[:todos] << {name: text, completed: false}
+    session[:success] = 'You added a todo'
+    redirect "/lists/#{@list_id}"
+  end
+end
+
+# Delete todo list
+post '/lists/:list_id/delete list' do
+  @list_id = params[:list_id].to_i
+  session[:lists].delete_at @list_id
+  session[:success] = 'The list has been deleted'
+  redirect "/lists"
+end
+
+# Delete todo item
+post '/lists/:list_id/delete item' do
+  @list_id = params[:list_id].to_i
+  todo_id = params[:todo_id].to_i
+  session[:lists][@list_id][:todos].delete_at todo_id
+  session[:success] = 'The todo item has been deleted'
+  redirect "/lists/#{@list_id}"
 end
 
 # Submit updated list
 post '/lists/:id' do
-  list_name = params[:list_name].strip
-  id = params[:id].to_i
-  @lists = session[:lists][id]
-  
+
+  list_name = params[:todo].strip
+  @list_id = params[:id].to_i
+  @lists = session[:lists][@list_id]
+
   error = validate(list_name)
   if error  
     session[:error] = error
@@ -72,29 +126,6 @@ post '/lists/:id' do
   else
     @lists[:name] = list_name
     session[:success] = 'The list has been updated!'
-    redirect "/lists/#{id}"
+    redirect "/lists/#{@list_id}"
   end
-end
-
-# Visit curent todo
-get '/lists/:id' do
-  @id = params[:id].to_i
-  @lists = session[:lists][@id]
-  erb :list
-end
-
-# Delete todo item
-post '/lists/:id/delete' do
-  id = params[:id].to_i
-  session[:lists].delete_at id
-  session[:success] = 'The list has been deleted'
-  redirect "/lists"
-end
-
-# Add todo item
-post '/lists/:id/todos' do
-  id = params[:id].to_i
-  session[:lists][id][:todos] << params[:todo] 
-  session[:success] = 'You added a todo'
-  redirect "/lists"
 end
